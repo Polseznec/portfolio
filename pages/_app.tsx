4b6d7294f2c9694e0020f7b86de9d30e584efa78
Pol Szc <seznecpol@gmail.com>
2023-06-19 00:32:10 +0200

    wip : init tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AppProps } from "next/app";
 
-import "../styles/global.scss";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
